92fdb20ccb1a3f24d8517f8448353488748e88bc
ignacio <idmp1998@gmail.com>
2024-04-11 14:36:52 -0300

    clase 2

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+       public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
